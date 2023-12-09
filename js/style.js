5a850c2276229d2baabc271ba97576516f45c2b0
catalinbroinas <catalinbroinas@yahoo.com>
2023-12-09 19:09:15 +0200

    Add JS passwordValidate() Check that the password has been entered correctly in both entries

--- js/style.js
+++ js/style.js
@@ -1,6 +1,9 @@
+const infoAlert = document.querySelector('.alert');
 const passwordClass = document.querySelectorAll('.password-control');
+
 passwordClass.forEach((element) => {
     element.addEventListener('input', () => {
+        infoAlert.style.cssText = 'display: none;';
         let str = element.value;
         if (str.length === 0) {
             if (element.classList.contains('valid')) {
@@ -23,4 +26,35 @@ passwordClass.forEach((element) => {
             element.classList.add('valid');
         }
     });
+});
+
+function passwordValidate() {
+    const password = document.querySelector('#password');
+    const passwordConfirm = document.querySelector('#confirm-password');
+
+    if (password.value != passwordConfirm.value) {
+        passwordClass.forEach((element) => {
+            if (element.classList.contains('valid')) {
+                element.classList.remove('valid');
+                element.classList.add('invalid');
+            }
+        });
+        infoAlert.style.cssText = 'display: block;';
+        infoAlert.textContent = 'Please confirm your password!';
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+const submitButton = document.querySelector('#btn-submit');
+submitButton.addEventListener('click', (event) => {
+    if (!passwordValidate()) {
+        event.preventDefault();
+    }
+    else {
+        event.preventDefault();
+        location.reload();
+    }
 });
